da6fd381c6d8e804592592af82f69983080f3d96
Lev Iakimov <ls.yakimov@gmail.com>
2025-11-10 00:49:57 +0100

    Task 3. Make measureNetworkSpeed

--- src/main/kotlin/ru/otus/homework/speedtest/speedtest.kt
+++ src/main/kotlin/ru/otus/homework/speedtest/speedtest.kt
@@ -1,5 +1,7 @@
 package ru.otus.homework.speedtest
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 
@@ -12,6 +14,19 @@ fun main(): Unit = runBlocking {
 }
 
 suspend fun measureNetworkSpeed(numOfRequests: Int, service: SpeedtestService): Double = coroutineScope {
-    TODO("Implement speed-test")
+    val successfulMeasurements = (0 until numOfRequests)
+        .map {
+            async {
+                service.measureRequestTime()
+            }
+        }
+        .awaitAll()
+        .mapNotNull { it.getOrNull() }
+
+    if (successfulMeasurements.isEmpty()) {
+        Double.NaN
+    } else {
+        successfulMeasurements.average()
+    }
 }
 
